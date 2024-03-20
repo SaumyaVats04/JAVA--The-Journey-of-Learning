6acf2b58f20f7abdc5989d448901b97567e0e05d
SaumyaVats04 <163171396+SaumyaVats04@users.noreply.github.com>
2024-03-20 22:47:27 +0530

    Add files via upload

---
+++ Code20.java
@@ -0,0 +1,13 @@
+// Date:20-03-2024 ,This is the journey of learning
+//CodeWithSaumya: Code14
+//Operators
+//Demonstrate the % operator
+
+public class Code20 {
+    public static void main(String[] args) {
+        int x = 42;
+        double y = 42.25;
+        System.out.println("x mod 10 = " + x % 10);
+        System.out.println("y mod 10 = " + y % 10);
+    }
+}
